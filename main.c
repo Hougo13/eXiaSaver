786ccf24feff124af283d5be8204a28097bcb0da
NooGat <kinghyde111@gmail.com>
2016-12-07 15:28:53 +0100

    Fonction extractFile (finie !)

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("Hello World");
+
+#include "libs/render.c"
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+
+
+    char NOM[30];
+    printf("Entrez le nom du fichier: ");
+    scanf("%s", &NOM);
+
+    extractFile(NOM);
+
     return 0;
 }
